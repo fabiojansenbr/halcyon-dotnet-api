366f1b5d19b726d5094cd4eb3d1812f90038dcd6
Chris Poulter <cpoulter@hotmail.co.uk>
2019-05-13 08:50:01 +0100

    code clean up

--- Halcyon.Api/build.ps1
+++
@@ -1,9 +0,0 @@
-dotnet tool install --global Amazon.Lambda.Tools
-
-pushd Halcyon.Api
-
-dotnet restore
-dotnet lambda package --configuration release --framework netcoreapp2.1 --output-package bin/release/netcoreapp2.1/deploy-package.zip
-serverless deploy
-
-popd
