1711c098eaaddadee3e52dae32c86d7f74bd8dff
Chris Poulter <cpoulter@hotmail.co.uk>
2019-05-14 09:28:10 +0100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install aws cli
+sudo pip3 install --upgrade pip
+pip3 install awscli --upgrade --user
+export PATH="/home/vsts/.local/bin:$PATH"
+
+# install dotnet lambda tools
+dotnet tool install --global Amazon.Lambda.Tools
+
+pushd Halcyon.Api
+
+dotnet restore
+dotnet lambda package --configuration release --framework netcoreapp2.1 --output-package bin/release/netcoreapp2.1/deploy-package.zip
+
+# deploy serverless project
+npx serverless deploy --stage $AWS_STAGE --region $AWS_DEFAULT_REGION
+
+popd
